d5cba6235bf158869e812e4873d6cbc9b86e237c
Charles Gibault <charles.gibault@gmail.com>
2012-09-05 23:47:03 +0200

    Inited multi-project architecture

---
+++ static-ioc-maven-plugin/src/main/java/org/staticioc/StaticIocMojo.java
@@ -0,0 +1,85 @@
+package org.staticioc;
+
+/**
+ * Static IoC - Compile XML based inversion of control configuration file into a single init class, for many languages.
+ * Project Home : http://code.google.com/p/static-ioc/
+ * 
+ *   Copyright 2012 Charles Gibault
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Convert IoC configuration file into source code that can be part of the compile process
+ * 
+ * @goal generate
+ * @phase generate-sources
+ * @requiresDependencyResolution compile
+ * @author <a href="https://code.google.com/p/static-ioc/">Charles Gibault</a>
+ * @version $Id$
+ */
+public class StaticIocMojo
+    extends AbstractMojo
+{
+    /**
+     * Location of the file.
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private File outputDirectory;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        File f = outputDirectory;
+
+        if ( !f.exists() )
+        {
+            f.mkdirs();
+        }
+
+        File touch = new File( f, "touch.txt" );
+
+        FileWriter w = null;
+        try
+        {
+            w = new FileWriter( touch );
+
+            w.write( "touch.txt" );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Error creating file " + touch, e );
+        }
+        finally
+        {
+            if ( w != null )
+            {
+                try
+                {
+                    w.close();
+                }
+                catch ( IOException e )
+                {
+                    // ignore
+                }
+            }
+        }
+    }
+}
