7770b603fec54919bc0c3dad39c52213062e6c12
charles <charles.gibault@gmail.com>
2013-07-10 01:11:00 +0200

    [Core] Implement some recommendations from PMD report

--- static-ioc-core/src/main/java/org/staticioc/SpringConfigParser.java
+++ static-ioc-core/src/main/java/org/staticioc/SpringConfigParser.java
@@ -419,11 +419,11 @@ public class SpringConfigParser extends AbstractSpringConfigParser
 	 * @throws SAXException
 	 * @throws IOException
 	 */
-	protected Map<String, Bean> load( String configurationFile, Set<String> loadedContext, boolean resolveBean) throws SAXException, IOException
+	protected Map<String, Bean> load( String entryFile, Set<String> loadedContext, boolean resolveBean) throws SAXException, IOException
 	{
 		// Start by keeping track of loaded file (after normalizing its name)
-		final String fullPath = FilenameUtils.getFullPath( configurationFile ) ; 
-		configurationFile = FilenameUtils.concat( fullPath, FilenameUtils.getName( configurationFile ) );
+		final String fullPath = FilenameUtils.getFullPath( entryFile ) ; 
+		final String configurationFile = FilenameUtils.concat( fullPath, FilenameUtils.getName( entryFile ) );
 		loadedContext.add( configurationFile);
 
 		final Document confFileDom = db.parse( configurationFile );
