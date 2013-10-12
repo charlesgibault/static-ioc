96bc227f1ec9b43386560fb325220aebc521f17c
charles <charles.gibault@gmail.com>
2013-10-12 15:35:04 +0200

    [Sample][GWT] Use display attribute for activity manager + XSD definition

--- static-ioc-samples/staticioc-samples-gwt/src/main/java/org/staticioc/samples/gwt/client/SampleGwtApplication.java
+++ static-ioc-samples/staticioc-samples-gwt/src/main/java/org/staticioc/samples/gwt/client/SampleGwtApplication.java
@@ -23,7 +23,6 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceHistoryHandler;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -33,20 +32,16 @@ public class SampleGwtApplication implements EntryPoint {
 	 * Load our application context once and for all
 	 */
 	private final ApplicationContext context = new ApplicationContext();
-	private SimplePanel appWidget = new SimplePanel();
 	private Place defaultPlace = new EditPlace("contacts");
 
 	/**
 	 * This is the entry point method. Very simple thanks to MVP + IoC couple
 	 */
-	public void onModuleLoad() {
-		// Use RootPanel.get() to get the entire body element
-	    context.activityManager.setDisplay(appWidget);
-		
+	public void onModuleLoad() {	
 	    PlaceHistoryHandler historyHandler = new PlaceHistoryHandler(context.historyMapper);
         historyHandler.register(context.placeController, context.eventBus, defaultPlace);
 
-    	RootPanel.get("gwtAppContainer").add(appWidget);
+    	RootPanel.get("gwtAppContainer").add(context.appWidget);
         
 	    // Goes to the place represented on URL else default place
 	    historyHandler.handleCurrentHistory();
